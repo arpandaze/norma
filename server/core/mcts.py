9bb078ae0c1395333e58b3f537ca4ad028d241c0
javaclll <yugu.upadhyaya@gmail.com>
2022-08-07 15:25:32 +0545

    [created] mcts and model files

---
+++ server/core/mcts.py
@@ -0,0 +1,28 @@
+class MonteCarlo:
+    
+    def __init__(self):
+        self.state = None
+        self.priorProbability = None
+        self.turn = None
+        self.children = None
+        self.visitCount = 0
+        self.totalValueSum = 0
+        self.averageValueNode = 0
+    
+    def selectNode(self):
+        pass
+
+    def expandNode(self):
+        pass
+
+    def rollOut(self):
+        pass
+
+    def backPropagate(self):
+        pass
+
+    def perform(self):
+        pass
+
+    def recordData(self):
+        pass
---
+++ server/core/model.py
@@ -0,0 +1,13 @@
+class Model:
+    
+    def valueNetwork(self):
+        pass
+
+    def policyNetwork(self):
+        pass
+
+    def rlModel(self):
+        pass
+
+    def predict(self):
+        pass
