5924a6bfe2cfb376c193148c4366f505cc7e19d6
waterlulu12 <aatishshrestha123@gmail.com>
2022-12-01 19:09:21 +0545

    fixed win screen and commented print

--- norma/norma/model.py
+++ norma/norma/model.py
@@ -28,7 +28,7 @@ class MM:
   def goatMax(self,board: Baghchal,depth, maximizing_player=True):
 
     if depth == 0 or board.game_status_check().decided:
-      print(f'depth: {depth}, player: {maximizing_player}, result: Evaluating')
+      # print(f'depth: {depth}, player: {maximizing_player}, result: Evaluating')
       return self.evaluation(board, goat_eval=True), None
 
     if maximizing_player:
@@ -46,21 +46,21 @@ class MM:
         
         reward = result[0] + moveReward
 
-        print()
-        print()
-        print(f"Goat Move Current {i.move}.")
-        print()
-        print(f"Goat Move for Goat Maximizer at Depth {depth}.")
-        print(f"moveReward: {moveReward}.")
-        print(f"results: {result}.")
-        print(f"total reward: {reward} and maxValue: {maxValue}.")
+        # print()
+        # print()
+        # print(f"Goat Move Current {i.move}.")
+        # print()
+        # print(f"Goat Move for Goat Maximizer at Depth {depth}.")
+        # print(f"moveReward: {moveReward}.")
+        # print(f"results: {result}.")
+        # print(f"total reward: {reward} and maxValue: {maxValue}.")
         if reward > maxValue:
           maxValue = reward
           best_move = i.move
         
-        print(f"new maxValue: {maxValue}.")
-        print()
-        print()
+        # print(f"new maxValue: {maxValue}.")
+        # print()
+        # print()
       return maxValue, best_move
 
     else:
@@ -77,21 +77,21 @@ class MM:
 
         reward = result[0] - moveReward
 
-        print()
-        print()
-        print(f"Tiger Move Current {i.move}.")
-        print()
-        print(f"Tiger Move for Goat Maximizer at Depth {depth}.")
-        print(f"moveReward: {moveReward}.")
-        print(f"results: {result}.")
-        print(f"total reward: {reward} and minValue: {minValue}.")
+        # print()
+        # print()
+        # print(f"Tiger Move Current {i.move}.")
+        # print()
+        # print(f"Tiger Move for Goat Maximizer at Depth {depth}.")
+        # print(f"moveReward: {moveReward}.")
+        # print(f"results: {result}.")
+        # print(f"total reward: {reward} and minValue: {minValue}.")
         if reward < minValue:
           minValue = reward
           best_move = i.move
         
-        print(f"new minValue: {minValue}.")
-        print()
-        print()
+        # print(f"new minValue: {minValue}.")
+        # print()
+        # print()
       return minValue,best_move
 
   def tigerMax(self,board: Baghchal, depth, maximizing_player=True):
@@ -113,22 +113,22 @@ class MM:
         
         reward = (1.5 - depth/5) * (result[0] + moveReward)
         
-        print()
-        print()
-        print(f"Tiger Move Current {i.move}.")
-        print()
-        print(f"Tiger Move for Tiger Maximizer at Depth {depth}.")
-        print(f"moveReward: {moveReward}.")
-        print(f"results: {result}.")
-        print(f"total reward: {reward} and maxValue: {maxValue}.")
+        # print()
+        # print()
+        # print(f"Tiger Move Current {i.move}.")
+        # print()
+        # print(f"Tiger Move for Tiger Maximizer at Depth {depth}.")
+        # print(f"moveReward: {moveReward}.")
+        # print(f"results: {result}.")
+        # print(f"total reward: {reward} and maxValue: {maxValue}.")
         if reward > maxValue:
 
           maxValue = reward
           best_move = i.move
         
-        print(f"new maxValue: {maxValue}.")
-        print()
-        print()
+        # print(f"new maxValue: {maxValue}.")
+        # print()
+        # print()
       return maxValue, best_move
 
     else:
@@ -145,20 +145,20 @@ class MM:
 
         reward = (1.5 - depth/5) * (result[0] - moveReward)
 
-        print()
-        print()
-        print(f"Goat Move Current {i.move}.")
-        print()
-        print(f"Goat Move for Tiger Maximizer at Depth {depth}.")
-        print(f"moveReward: {moveReward}.")
-        print(f"results: {result}.")
-        print(f"total reward: {reward} and minValue: {minValue}.")
+        # print()
+        # print()
+        # print(f"Goat Move Current {i.move}.")
+        # print()
+        # print(f"Goat Move for Tiger Maximizer at Depth {depth}.")
+        # print(f"moveReward: {moveReward}.")
+        # print(f"results: {result}.")
+        # print(f"total reward: {reward} and minValue: {minValue}.")
         if reward < minValue:
           minValue = reward
           best_move = i.move
-        print(f"new minValue: {minValue}.")
-        print()
-        print()
+        # print(f"new minValue: {minValue}.")
+        # print()
+        # print()
       return minValue,best_move
 
   def best_bagh_move(self,board):
@@ -175,106 +175,6 @@ class MM:
     else:
       result = self.best_bagh_move(board)
 
-    print("The Result is: ", result)
+    # print("The Result is: ", result)
     return result
 
-# a = Minimax()
-# board = Baghchal.default()
-
-# while not board.game_status_check().decided:
-#   res = a.best_move(board)
-#   board.make_move(*res[1])
-
-# print(board.pgn())
-
-class AlphaBeta:
-
-  def __init__(self,depth=6):
-    self.depth = depth
-
-  def evaluation(self,board,turn):
-    
-    total_gcaptured = board.goat_captured()
-    total_tiger_trapped = board.trapped_tiger()
-    isOver = board.game_status_check().decided
-
-    if not isOver:
-      if turn == 1:
-        return 150 * total_tiger_trapped - 120 * total_gcaptured
-      else:
-        return 150 * total_gcaptured - 120 * total_tiger_trapped
-
-    winner = board.game_state()
-
-    if winner == 1:
-      return float('inf')
-    
-    elif winner == 2:
-      return float('-inf')
-    
-    else:
-      return 0
-
-  def alpha_beta(self,board,turn,depth=0,alpha=float('-inf'),beta=float('inf'),maximizing_player=True):
-    if depth == 0 or board.game_status_check().decided:
-      return self.evaluation(board,turn), None
-
-    if maximizing_player:
-      maxValue = float('-inf')
-      best_move = None
-      possible_moves = board.get_possible_moves()
-
-      for i in possible_moves:
-        board_cp = board.copy()
-        board_cp.make_move(*i.move) 
-
-        result = self.alpha_beta(board_cp,turn,depth-1,alpha,beta,False)
-
-        if result[0] > maxValue:
-          maxValue = result[0]
-          best_move = i.move
-          
-        alpha = max(alpha,result[0])
-
-        if beta <= alpha:
-          break
-
-      return maxValue, best_move
-
-    else:
-      minValue = float('inf')
-      best_move = None
-      possible_moves = board.get_possible_moves()
-
-      for i in possible_moves:
-        board_cp = board.copy()
-        board_cp.make_move(*i.move)
-        result = self.alpha_beta(board_cp,turn,depth-1,alpha,beta,True)
-
-        if result[0] < minValue:
-          minValue = result[0]
-          best_move = i.move
-
-        beta = min(beta,result[0])
-
-        if beta <= alpha:
-          break
-
-      return minValue,best_move
-
-  def best_bagh_move(self,board,turn):
-    assert not board.game_status_check().decided
-    return self.alpha_beta(board,turn,self.depth, maximizing_player=True) 
-    
-  def best_goat_move(self,board,turn):
-    assert not board.game_status_check().decided
-    return self.alpha_beta(board,turn,self.depth, maximizing_player=True)
-
-  def best_move(self,board:Baghchal):
-    if board.turn() == 1:
-      result = self.best_goat_move(board,turn=1)
-    else:
-      result = self.best_bagh_move(board,turn=-1)
-
-    print(result)
-    return result
