5c2e35dc544879033cb02fbbe53929e02460500e
Rushab11 <humagainrushab@gmail.com>
2023-06-19 11:08:45 +0545

    feat: plots with jitter reduced

--- policynorma/plot.py
+++ policynorma/plot.py
@@ -48,15 +48,19 @@ def plot_data_tiger():
     plt.rcParams["figure.autolayout"] = True
 
     data = pd.read_csv("tiger.csv")
+    data.pop("Predicted Invalid Moves")
+    data.pop("Turns")
 
-    wins = data["Wins"]
-    loss = data["Loss"]
-    draw = data["Draw"]
-    time = np.arange(len(data)) * 2
+    new_data = data[::5]
+
+    wins = new_data["Wins"]
+    loss = new_data["Loss"]
+    # draw = new_data["Draw"]
+    time = np.arange(len(new_data)) * 2 
 
     plt.plot(time, wins, label="Wins")
     plt.plot(time, loss, label="Loss")
-    plt.plot(time, draw, label="Draw")
+    # plt.plot(time, draw, label="Draw")
 
     plt.xlabel("Time Interval")
     plt.ylabel("Number of Games")
@@ -71,11 +75,14 @@ def plot_data_goat():
     plt.rcParams["figure.autolayout"] = True
 
     data = pd.read_csv("goat.csv")
+    data.pop("Predicted Invalid Moves")
+    data.pop("Turns")
+    new_data = data.rolling(8).min().iloc[::5]
 
-    wins = data["Wins"]
-    loss = data["Loss"]
-    draw = data["Draw"]
-    time = np.arange(len(data)) * 2
+    wins = new_data["Wins"]
+    loss = new_data["Loss"]
+    draw = new_data["Draw"]
+    time = np.arange(len(new_data)) * 2
 
     plt.plot(time, wins, label="Wins")
     plt.plot(time, loss, label="Loss")
@@ -89,7 +96,24 @@ def plot_data_goat():
 
     plt.savefig("goat.png", dpi=300, bbox_inches="tight")
 
+def plot_invalid_moves():
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
 
+    data = pd.read_csv("goat.csv")
+
+    invalid_moves = data["Predicted Invalid Moves"].apply(lambda x: [int(i) for i in eval(x)])
+    game_length = data["Turns"].apply(lambda x: [int(i) for i in eval(x)])
+
+    plt.title("Goat Wins, Loss and Draws Over Time")
+    plt.legend()
+
+    plt.xlabel("Time Interval")
+    plt.ylabel("Number of Games")
+
+    # plt.savefig("invalid.png", dpi=300, bbox_inches="tight")
+
+# plot_invalid_moves()
 separate_data("gameplayrecord.csv")
 plot_data_tiger()
 plt.clf()
